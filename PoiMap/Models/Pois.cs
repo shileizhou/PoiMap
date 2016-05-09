026aa6b6091b42ad9149937a3f094776176e8713
v5@outlook.com <zhuzhu88>
2016-05-09 17:02:16 -0400

    Poi

---
+++ PoiMap/Models/Pois.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoiMap.Models
+{
+    public class Pois
+    {
+
+        public string PlaceId { get; set; }
+        public string PlaceName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public decimal Longtitude { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Altitude { get; set; }
+        public string Icon { get; set; }
+        public string MainPhoto { get; set; }
+        public string Video { get; set; }
+        public string Wiki { get; set; }
+        public string ARName { get; set; }
+        public string ARPhoto { get; set; }
+        public string Website { get; set; }
+    
+    }
+
+}
+}
