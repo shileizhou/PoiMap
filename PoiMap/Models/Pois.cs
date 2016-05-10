8af8468b4d99302329f281fb566426563e266f5a
Richard Zhou <v5@outlook.com>
2016-05-10 11:25:52 -0400

    Insert

--- PoiMap/Models/Pois.cs
+++ PoiMap/Models/Pois.cs
@@ -2,12 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;  // System.Data.dll
+using System.Data;
 
 namespace PoiMap.Models
 {
+
+    public class PoisOps
+    {
+        public string InsertPoi(Pois InputPoi)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(
+                                          "Server=tcp:livemap.database.windows.net,1433;Database=livemap;User ID=livemap@livemap;Password=Zhuzhu88;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+                {
+                    connection.Open();
+
+                    using (SqlCommand cmd = new SqlCommand("AddPoi", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@PlaceId", InputPoi.PlaceId);
+                        cmd.Parameters.AddWithValue("@PlaceName", InputPoi.PlaceName);
+                        cmd.Parameters.AddWithValue("@City", InputPoi.City);
+                        cmd.Parameters.AddWithValue("@Country", InputPoi.Country);
+                        cmd.Parameters.AddWithValue("@Description", InputPoi.Description);
+                        cmd.Parameters.AddWithValue("@Longtitude", InputPoi.Longtitude);
+                        cmd.Parameters.AddWithValue("@Latitude", InputPoi.Latitude);
+                        cmd.Parameters.AddWithValue("@Altitude", InputPoi.Altitude);
+                        cmd.Parameters.AddWithValue("@Icon", InputPoi.Icon);
+                        cmd.Parameters.AddWithValue("@MainPhoto", InputPoi.MainPhoto);
+                        cmd.Parameters.AddWithValue("@Video", InputPoi.Video);
+                        cmd.Parameters.AddWithValue("@Wiki", InputPoi.Wiki);
+                        cmd.Parameters.AddWithValue("@ARName", InputPoi.ARName);
+
+                        cmd.ExecuteNonQuery();
+
+                    }
+
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return "Success";
+        }
+
+    }
     public class Pois
     {
-
         public string PlaceId { get; set; }
         public string PlaceName { get; set; }
         public string City { get; set; }
@@ -23,8 +69,7 @@ namespace PoiMap.Models
         public string ARName { get; set; }
         public string ARPhoto { get; set; }
         public string Website { get; set; }
-    
+
     }
 
-}
 }
